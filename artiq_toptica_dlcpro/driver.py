ad7d5dfeee4b075ec53d9aa38bdfb244c06d7e9a
Miłosz Malczak <milosz.malczak@gmail.com>
2026-02-24 23:14:51 +0100

    Add additional falc params

--- artiq_toptica_dlcpro/driver.py
+++ artiq_toptica_dlcpro/driver.py
@@ -67,6 +67,90 @@ class ArtiqTopticaDLCproInterface(abc.ABC):
         pass
 
     @abc.abstractmethod
+    async def get_falc_input_gain(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_input_gain_raw(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_input_offset(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_path_selection(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_enabled(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_i1(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_i1_raw(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_i1_enabled(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_i2(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_i2_raw(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_i2_enabled(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_i3(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_i3_raw(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_i3_enabled(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_d1(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_d1_raw(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_d1_enabled(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_d2(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_d2_raw(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain_d2_enabled(self, falc_number):
+        pass
+
+    @abc.abstractmethod
+    async def get_falc_main_gain(self, falc_number):
+        pass
+
+    @abc.abstractmethod
     async def get_laser_lock_status(self, channel):
         pass
 
@@ -224,6 +308,166 @@ class ArtiqTopticaDLCpro(ArtiqTopticaDLCproInterface):
         falc = self.get_falc(falc_number)
         return falc.status.get()
 
+    async def get_falc_input_gain(self, falc_number):
+        """
+        Get the gain of FALC's input.
+        """
+        gain_val = await self.get_falc_input_gain_raw(falc_number)
+        return 5 if gain_val == 1 else 1
+
+    async def get_falc_input_gain_raw(self, falc_number):
+        """
+        Get the raw gain value of FALC's input.
+        0 - 1x
+        1 - 5x
+        """
+        return self.raw_client.get(f"falc{falc_number}:input:gain")
+
+    async def get_falc_input_offset(self, falc_number):
+        """
+        Get the offset of FALC's input (in V).
+        """
+        return self.raw_client.get(f"falc{falc_number}:input:offset")
+
+    async def get_falc_path_selection(self, falc_number):
+        """
+        Get the FALC controller paths for \"lock-enabled\" and \"hold\" of the DLC pro lock machine.
+        """
+        return self.raw_client.get(f"falc{falc_number}:path-selection")
+
+    async def get_falc_main_enabled(self, falc_number):
+        """
+        Get Lock-ON/OFF for FALC's main path.
+        """
+        return self.raw_client.get(f"falc{falc_number}:main:enabled")
+
+    i1_freqs = {
+        1: 1.5e3, 2: 3.0e3, 3: 7.0e3, 4: 15e3, 5: 31e3,
+        6: 70e3, 7: 130e3, 8: 290e3, 9: 620e3, 10: 7.0e6
+    }
+    
+    i2_freqs = {
+        1: 25, 2: 50, 3: 100, 4: 220, 5: 470,
+        6: 1.0e3, 7: 2.2e3, 8: 5.0e3, 9: 10e3
+    }
+    
+    i3_freqs = {
+        1: 0.6, 2: 1.8, 3: 6.0, 4: 18, 5: 60,
+        6: 180, 7: 600
+    }
+    
+    d1_freqs = {
+        1: 10e3, 2: 20e3, 3: 40e3, 4: 90e3, 5: 190e3,
+        6: 400e3, 7: 760e3, 8: 1.5e6, 9: 3.5e6, 10: 7.2e6
+    }
+    
+    d2_freqs = {
+        1: 10e3, 2: 20e3, 3: 45e3, 4: 100e3, 5: 200e3,
+        6: 420e3, 7: 700e3, 8: 1.2e6, 9: 3.5e6, 10: 6.0e6
+    }
+
+    async def get_falc_main_gain_i1(self, falc_number):
+        """
+        Get the corner frequency of the main path's I1 in Hz.
+        """
+        val = await self.get_falc_main_gain_i1_raw(falc_number)
+        return self.i1_freqs.get(val, val)
+
+    async def get_falc_main_gain_i1_raw(self, falc_number):
+        """
+        Get the raw corner frequency integer of the main path's I1.
+        """
+        return self.raw_client.get(f"falc{falc_number}:main:gain:i1")
+
+    async def get_falc_main_gain_i1_enabled(self, falc_number):
+        """
+        Get whether the main path's I1 is enabled.
+        """
+        return self.raw_client.get(f"falc{falc_number}:main:gain:i1-enabled")
+
+    async def get_falc_main_gain_i2(self, falc_number):
+        """
+        Get the corner frequency of the main path's I2 in Hz.
+        """
+        val = await self.get_falc_main_gain_i2_raw(falc_number)
+        return self.i2_freqs.get(val, val)
+
+    async def get_falc_main_gain_i2_raw(self, falc_number):
+        """
+        Get the raw corner frequency integer of the main path's I2.
+        """
+        return self.raw_client.get(f"falc{falc_number}:main:gain:i2")
+
+    async def get_falc_main_gain_i2_enabled(self, falc_number):
+        """
+        Get whether the main path's I2 is enabled.
+        """
+        return self.raw_client.get(f"falc{falc_number}:main:gain:i2-enabled")
+
+    async def get_falc_main_gain_i3(self, falc_number):
+        """
+        Get the corner frequency of the main path's I3 in Hz.
+        """
+        val = await self.get_falc_main_gain_i3_raw(falc_number)
+        return self.i3_freqs.get(val, val)
+
+    async def get_falc_main_gain_i3_raw(self, falc_number):
+        """
+        Get the raw corner frequency integer of the main path's I3.
+        """
+        return self.raw_client.get(f"falc{falc_number}:main:gain:i3")
+
+    async def get_falc_main_gain_i3_enabled(self, falc_number):
+        """
+        Get whether the main path's I3 is enabled.
+        """
+        return self.raw_client.get(f"falc{falc_number}:main:gain:i3-enabled")
+
+    async def get_falc_main_gain_d1(self, falc_number):
+        """
+        Get the corner frequency of the main path's D1 in Hz.
+        """
+        val = await self.get_falc_main_gain_d1_raw(falc_number)
+        return self.d1_freqs.get(val, val)
+
+    async def get_falc_main_gain_d1_raw(self, falc_number):
+        """
+        Get the raw corner frequency integer of the main path's D1.
+        """
+        return self.raw_client.get(f"falc{falc_number}:main:gain:d1")
+
+    async def get_falc_main_gain_d1_enabled(self, falc_number):
+        """
+        Get whether the main path's D1 is enabled.
+        """
+        return self.raw_client.get(f"falc{falc_number}:main:gain:d1-enabled")
+
+    async def get_falc_main_gain_d2(self, falc_number):
+        """
+        Get the corner frequency of the main path's D2 in Hz.
+        """
+        val = await self.get_falc_main_gain_d2_raw(falc_number)
+        return self.d2_freqs.get(val, val)
+
+    async def get_falc_main_gain_d2_raw(self, falc_number):
+        """
+        Get the raw corner frequency integer of the main path's D2.
+        """
+        return self.raw_client.get(f"falc{falc_number}:main:gain:d2")
+
+    async def get_falc_main_gain_d2_enabled(self, falc_number):
+        """
+        Get whether the main path's D2 is enabled.
+        """
+        return self.raw_client.get(f"falc{falc_number}:main:gain:d2-enabled")
+
+
+    async def get_falc_main_gain(self, falc_number):
+        """
+        Get the gain of FALC's main path.
+        """
+        return self.raw_client.get(f"falc{falc_number}:main:gain:all")
+
     async def get_laser_lock_status(self, channel):
         """
         To see if the system is properly locked.
@@ -278,6 +522,23 @@ class ArtiqTopticaDLCproSim(ArtiqTopticaDLCproInterface):
         # New parameters initialization
         self.falc_temperature = 2 * [25.0]
         self.falc_status = 2 * [0]
+
+        self.falc_input_gain = 2 * [0]
+        self.falc_input_offset = 2 * [0.0]
+        self.falc_path_selection = 2 * [0]
+
+        self.falc_main_enabled = 2 * [True]
+        self.falc_main_gain_i1 = 2 * [7]
+        self.falc_main_gain_i1_enabled = 2 * [True]
+        self.falc_main_gain_i2 = 2 * [7]
+        self.falc_main_gain_i2_enabled = 2 * [True]
+        self.falc_main_gain_i3 = 2 * [1]
+        self.falc_main_gain_i3_enabled = 2 * [False]
+        self.falc_main_gain_d1 = 2 * [6]
+        self.falc_main_gain_d1_enabled = 2 * [True]
+        self.falc_main_gain_d2 = 2 * [10]
+        self.falc_main_gain_d2_enabled = 2 * [True]
+        self.falc_main_gain = 2 * [-6.5]
 
         self.laser_lock_status = 2 * [0]
         self.cavity_temperature = 22.5
@@ -391,6 +652,123 @@ class ArtiqTopticaDLCproSim(ArtiqTopticaDLCproInterface):
             f"{self.falc_status[conv_falc]}"
         )
         return self.falc_status[conv_falc]
+
+    async def get_falc_input_gain(self, falc_number):
+        val = await self.get_falc_input_gain_raw(falc_number)
+        converted = 5 if val == 1 else 1
+        logging.warning(f"Simulated: Falc {falc_number} input gain redout {converted}")
+        return converted
+
+    async def get_falc_input_gain_raw(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        val = self.falc_input_gain[conv_falc]
+        logging.warning(f"Simulated: Falc {falc_number} input gain raw redout {val}")
+        return val
+
+    async def get_falc_input_offset(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        logging.warning(f"Simulated: Falc {falc_number} input offset redout {self.falc_input_offset[conv_falc]}")
+        return self.falc_input_offset[conv_falc]
+
+    async def get_falc_path_selection(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        logging.warning(f"Simulated: Falc {falc_number} path selection redout {self.falc_path_selection[conv_falc]}")
+        return self.falc_path_selection[conv_falc]
+
+    async def get_falc_main_enabled(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        logging.warning(f"Simulated: Falc {falc_number} main enabled redout {self.falc_main_enabled[conv_falc]}")
+        return self.falc_main_enabled[conv_falc]
+
+    async def get_falc_main_gain_i1(self, falc_number):
+        val = await self.get_falc_main_gain_i1_raw(falc_number)
+        converted = ArtiqTopticaDLCpro.i1_freqs.get(val, val)
+        logging.warning(f"Simulated: Falc {falc_number} main gain i1 redout {converted}")
+        return converted
+
+    async def get_falc_main_gain_i1_raw(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        val = self.falc_main_gain_i1[conv_falc]
+        logging.warning(f"Simulated: Falc {falc_number} main gain i1 raw redout {val}")
+        return val
+
+    async def get_falc_main_gain_i1_enabled(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        logging.warning(f"Simulated: Falc {falc_number} main gain i1 enabled redout {self.falc_main_gain_i1_enabled[conv_falc]}")
+        return self.falc_main_gain_i1_enabled[conv_falc]
+
+    async def get_falc_main_gain_i2(self, falc_number):
+        val = await self.get_falc_main_gain_i2_raw(falc_number)
+        converted = ArtiqTopticaDLCpro.i2_freqs.get(val, val)
+        logging.warning(f"Simulated: Falc {falc_number} main gain i2 redout {converted}")
+        return converted
+
+    async def get_falc_main_gain_i2_raw(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        val = self.falc_main_gain_i2[conv_falc]
+        logging.warning(f"Simulated: Falc {falc_number} main gain i2 raw redout {val}")
+        return val
+
+    async def get_falc_main_gain_i2_enabled(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        logging.warning(f"Simulated: Falc {falc_number} main gain i2 enabled redout {self.falc_main_gain_i2_enabled[conv_falc]}")
+        return self.falc_main_gain_i2_enabled[conv_falc]
+
+    async def get_falc_main_gain_i3(self, falc_number):
+        val = await self.get_falc_main_gain_i3_raw(falc_number)
+        converted = ArtiqTopticaDLCpro.i3_freqs.get(val, val)
+        logging.warning(f"Simulated: Falc {falc_number} main gain i3 redout {converted}")
+        return converted
+
+    async def get_falc_main_gain_i3_raw(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        val = self.falc_main_gain_i3[conv_falc]
+        logging.warning(f"Simulated: Falc {falc_number} main gain i3 raw redout {val}")
+        return val
+
+    async def get_falc_main_gain_i3_enabled(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        logging.warning(f"Simulated: Falc {falc_number} main gain i3 enabled redout {self.falc_main_gain_i3_enabled[conv_falc]}")
+        return self.falc_main_gain_i3_enabled[conv_falc]
+
+    async def get_falc_main_gain_d1(self, falc_number):
+        val = await self.get_falc_main_gain_d1_raw(falc_number)
+        converted = ArtiqTopticaDLCpro.d1_freqs.get(val, val)
+        logging.warning(f"Simulated: Falc {falc_number} main gain d1 redout {converted}")
+        return converted
+        
+    async def get_falc_main_gain_d1_raw(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        val = self.falc_main_gain_d1[conv_falc]
+        logging.warning(f"Simulated: Falc {falc_number} main gain d1 raw redout {val}")
+        return val
+
+    async def get_falc_main_gain_d1_enabled(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        logging.warning(f"Simulated: Falc {falc_number} main gain d1 enabled redout {self.falc_main_gain_d1_enabled[conv_falc]}")
+        return self.falc_main_gain_d1_enabled[conv_falc]
+
+    async def get_falc_main_gain_d2(self, falc_number):
+        val = await self.get_falc_main_gain_d2_raw(falc_number)
+        converted = ArtiqTopticaDLCpro.d2_freqs.get(val, val)
+        logging.warning(f"Simulated: Falc {falc_number} main gain d2 redout {converted}")
+        return converted
+        
+    async def get_falc_main_gain_d2_raw(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        val = self.falc_main_gain_d2[conv_falc]
+        logging.warning(f"Simulated: Falc {falc_number} main gain d2 raw redout {val}")
+        return val
+
+    async def get_falc_main_gain_d2_enabled(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        logging.warning(f"Simulated: Falc {falc_number} main gain d2 enabled redout {self.falc_main_gain_d2_enabled[conv_falc]}")
+        return self.falc_main_gain_d2_enabled[conv_falc]
+
+    async def get_falc_main_gain(self, falc_number):
+        conv_falc = self.convert_falc(falc_number)
+        logging.warning(f"Simulated: Falc {falc_number} main gain redout {self.falc_main_gain[conv_falc]}")
+        return self.falc_main_gain[conv_falc]
 
     async def get_laser_lock_status(self, channel):
         conv_channel = self.convert_channel(channel)
--- artiq_toptica_dlcpro/test_artiq_toptica_dlcpro.py
+++ artiq_toptica_dlcpro/test_artiq_toptica_dlcpro.py
@@ -53,9 +53,32 @@ class GenericTopticaDLCproTest:
         self.assertEqual(25.0, self.artiq_toptica_dlcpro.get_falc_temperature(falc_num))
         self.assertEqual(0, self.artiq_toptica_dlcpro.get_falc_status(falc_num))
 
+        self.assertEqual(0, self.artiq_toptica_dlcpro.get_falc_input_gain_raw(falc_num))
+        self.assertEqual(1, self.artiq_toptica_dlcpro.get_falc_input_gain(falc_num))
+        self.assertEqual(0.0, self.artiq_toptica_dlcpro.get_falc_input_offset(falc_num))
+        self.assertEqual(0, self.artiq_toptica_dlcpro.get_falc_path_selection(falc_num))
+
+        self.assertEqual(True, self.artiq_toptica_dlcpro.get_falc_main_enabled(falc_num))
+        self.assertEqual(7, self.artiq_toptica_dlcpro.get_falc_main_gain_i1_raw(falc_num))
+        self.assertEqual(130e3, self.artiq_toptica_dlcpro.get_falc_main_gain_i1(falc_num))
+        self.assertEqual(True, self.artiq_toptica_dlcpro.get_falc_main_gain_i1_enabled(falc_num))
+        self.assertEqual(7, self.artiq_toptica_dlcpro.get_falc_main_gain_i2_raw(falc_num))
+        self.assertEqual(2.2e3, self.artiq_toptica_dlcpro.get_falc_main_gain_i2(falc_num))
+        self.assertEqual(True, self.artiq_toptica_dlcpro.get_falc_main_gain_i2_enabled(falc_num))
+        self.assertEqual(1, self.artiq_toptica_dlcpro.get_falc_main_gain_i3_raw(falc_num))
+        self.assertEqual(0.6, self.artiq_toptica_dlcpro.get_falc_main_gain_i3(falc_num))
+        self.assertEqual(False, self.artiq_toptica_dlcpro.get_falc_main_gain_i3_enabled(falc_num))
+        self.assertEqual(6, self.artiq_toptica_dlcpro.get_falc_main_gain_d1_raw(falc_num))
+        self.assertEqual(400e3, self.artiq_toptica_dlcpro.get_falc_main_gain_d1(falc_num))
+        self.assertEqual(True, self.artiq_toptica_dlcpro.get_falc_main_gain_d1_enabled(falc_num))
+        self.assertEqual(10, self.artiq_toptica_dlcpro.get_falc_main_gain_d2_raw(falc_num))
+        self.assertEqual(6.0e6, self.artiq_toptica_dlcpro.get_falc_main_gain_d2(falc_num))
+        self.assertEqual(True, self.artiq_toptica_dlcpro.get_falc_main_gain_d2_enabled(falc_num))
+        self.assertEqual(-6.5, self.artiq_toptica_dlcpro.get_falc_main_gain(falc_num))
+
     def test_get_laser_parameters(self):
         channel = 1
-        self.assertEqual(True, self.artiq_toptica_dlcpro.get_laser_emission(channel))
+        self.assertEqual(True, self.artiq_toptica_dlcpro.get_emission())
         self.assertEqual(0, self.artiq_toptica_dlcpro.get_laser_lock_status(channel))
         self.assertEqual(26.0, self.artiq_toptica_dlcpro.get_amplifier_temperature(channel))
         self.assertEqual(100.0, self.artiq_toptica_dlcpro.get_amplifier_current(channel))
